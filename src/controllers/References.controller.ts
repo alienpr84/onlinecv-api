e2062b90679e7623f034cef0a0f30aa76933d46c
Alien Padilla Rodriguez <alienpr84@gmail.com>
2021-11-18 17:20:23 -0500

    references' controller implemented

--- src/controllers/References.controller.ts
+++ src/controllers/References.controller.ts
@@ -1,26 +1,77 @@
 import { Request, Response } from 'express';
 import BaseController from 'src/interfaces/BaseController.interface';
+import ReferenceModel from 'src/models/Reference.model';
 
 const controller: BaseController = {} as BaseController;
 
 controller.create = async (req: Request, res: Response) => {
-	res.json({ message: 'create' });
+	try {
+		const model = new ReferenceModel(req.body);
+		await model.save();
+		res.status(201).json({
+			message: 'created',
+			payload: model,
+		});
+	} catch (error: unknown) {
+		res.status(403).json({
+			message: error instanceof Error ? error.message : String(error),
+		});
+	}
 };
 
 controller.getAll = async (req: Request, res: Response) => {
-	res.json({ message: 'getAll' });
+	try {
+		const models = await ReferenceModel.find();
+		res.status(200).json({
+			message: 'success',
+			payload: models,
+		});
+	} catch (error: unknown) {
+		res.status(403).json({
+			message: error instanceof Error ? error.message : String(error),
+		});
+	}
 };
 
 controller.getById = async (req: Request, res: Response) => {
-	res.json({ message: 'getById' });
+	try {
+		const model = await ReferenceModel.findById(req.params.id);
+		res.status(200).json({
+			message: 'success',
+			payload: model,
+		});
+	} catch (error: unknown) {
+		res.status(403).json({
+			message: error instanceof Error ? error.message : String(error),
+		});
+	}
 };
 
 controller.update = async (req: Request, res: Response) => {
-	res.json({ message: 'update' });
+	try {
+		const model = await ReferenceModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+		res.status(200).json({
+			message: 'udpated',
+			payload: model,
+		});
+	} catch (error: unknown) {
+		res.status(403).json({
+			message: error instanceof Error ? error.message : String(error),
+		});
+	}
 };
 
 controller.delete = async (req: Request, res: Response) => {
-	res.json({ message: 'delete' });
+	try {
+		await ReferenceModel.findByIdAndDelete(req.params.id);
+		res.status(204).json({
+			message: 'deleted',
+		});
+	} catch (error: unknown) {
+		res.status(403).json({
+			message: error instanceof Error ? error.message : String(error),
+		});
+	}
 };
 
 export default controller;
